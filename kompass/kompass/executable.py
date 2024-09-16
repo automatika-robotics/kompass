026eb7db09894e2bf6b3e789a7574aaa54db8cae
mkabtoul <kabtoul.maria@gmail.com>
2024-09-16 14:05:16 +0200

    Moves components and config lists to executable

--- kompass/kompass/executable.py
+++ kompass/kompass/executable.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 from ros_sugar import executable_main
-from kompass import components_list, configs_list
+from kompass.components import (
+    Controller,
+    ControllerConfig,
+    DriveManager,
+    DriveManagerConfig,
+    MotionServer,
+    MotionServerConfig,
+    Planner,
+    PlannerConfig,
+)
+
+_kompass_components = [Planner, Controller, DriveManager, MotionServer]
+
+_kompass_configs = [
+    PlannerConfig,
+    ControllerConfig,
+    DriveManagerConfig,
+    MotionServerConfig,
+]
 
 
 def main(args=None):
@@ -17,9 +35,6 @@ def main(args=None):
                                                 arguments=component.launch_cmd_args,
                                             )
 
-    :param args: _description_, defaults to None
-    :type args: _type_, optional
-
     :raises ValueError: If component cannot be started with provided arguments
     """
-    executable_main(components_list, configs_list)
+    executable_main(list_of_components=_kompass_components, list_of_configs=_kompass_configs)
