932a638f45ed4ef2df01d914eccc91aa0ae7dc2d
ahr <ahrasheed@gmail.com>
2025-02-18 12:53:28 +0100

    (fix) Updates installation instructions for kompass-core if not found

--- kompass/kompass/__init__.py
+++ kompass/kompass/__init__.py
@@ -4,8 +4,7 @@
 try:
     import kompass_core
 except ImportError:
-    # TODO: Adds GPU install string
     print(
-        "Welcome to Kompass! The fastest and most intuitive navigation stack known to man! Kompass uses the `kompass-core` python package, which implements highly paralellized navigation algorithms. Please install kompass-core using one of the following methods:\n - Install kompass-core with GPU support (Recommended):\n  `installation string`\n - Install kompass-core from pypi without GPU support:\n  `pip install kompass-core`"
+        "Welcome to Kompass! The fastest and most intuitive navigation stack known to man! Kompass uses the `kompass-core` python package, which implements highly paralellized navigation algorithms. Please install kompass-core using one of the following methods:\n - Install kompass-core with GPU support (Recommended):\n  `curl https://raw.githubusercontent.com/automatika-robotics/kompass-core/refs/heads/main/build_dependencies/install_gpu.sh | bash`\n - Install kompass-core from pypi without GPU support:\n  `sudo apt-get install -y libompl-dev libfcl-dev libpcl-dev && pip install kompass-core`"
     )
     exit(1)
